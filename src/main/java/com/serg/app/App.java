624513ec5f24061a39bc825c9b81084e1c75e4bf
Sergman <s_linux@inbox.ru>
2013-07-08 21:33:07 +0300

    First commit

--- src/main/java/com/serg/app/App.java
+++ src/main/java/com/serg/app/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+    }
+
+    public void run() {
+        System.out.println( "Hello App!" );
     }
 }
